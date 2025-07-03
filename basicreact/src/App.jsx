842d57c2d390de2248098a920d1155f41da21568
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-07-03 22:05:56 +0600

    installing vite react

---
+++ basicreact/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Code with Ashraful</h1>
+        <button>BOOM</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
